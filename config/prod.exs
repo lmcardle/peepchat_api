245609d5b15010ac8ff8e7f848cf82e6de8f7b89
Liam McArdle <mcardle.liam@gmail.com>
2016-12-23 06:39:59 -0800

    changes to config/prod.ex

--- config/prod.exs
+++ config/prod.exs
@@ -14,14 +14,15 @@ use Mix.Config
 config :peepchat, Peepchat.Endpoint,
   http: [port: {:system, "PORT"}],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
-  url: [host: "fast-lowlands-52109.herokuapp.com", port: 443],
+  url: [scheme: "https", host: "fast-lowlands-52109.herokuapp.com", port: 443],
   secret_key_base: System.get_env("SECRET_KEY_BASE"),
   cache_static_manifest: "priv/static/manifest.json"
 
 config :peepchat, Peepchat.Repo,
   adapter: Ecto.Adapters.Postgres,
   url: System.get_env("DATABASE_URL"),
-  pool_size: 20
+  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "20"),
+  ssl: true
 
 # Do not print debug messages in production
 config :logger, level: :info
